8c969725cc2dfa3e23af109ae39f14701157cdd6
fafakerasvhilhi <giorgi.fafa@gmail.com>
2015-06-15 17:27:48 +0400

    ma

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello")
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
